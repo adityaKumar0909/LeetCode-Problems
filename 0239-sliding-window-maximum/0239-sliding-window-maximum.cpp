dd6f092aa18d5fbd68c390153f0bb7fcc297793f
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-16 21:17:59 +0530

    Time: 2372 ms (5.04%), Space: 141.4 MB (28.51%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+
+
+        vector<int> ans;
+
+        if(k==1){
+            for(auto x:arr){
+                ans.push_back(x);
+            }
+            return ans;
+        }
+        deque<pair<int, int>> dq;
+        int index = 0;
+        int max = arr[0];
+
+        // Precomputing for first k window size
+        for (int i = 0; i < k; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+
+        dq.emplace_back(index, arr[index]);
+        ans.push_back(arr[index]);
+
+        for (int i = index + 1; i < k; i++) {
+            while (!dq.empty() && dq.back().second < arr[i]) {
+                dq.pop_back();
+            }
+
+            dq.emplace_back(i, arr[i]);
+        }
+
+        for (auto x : dq) {
+            cout << x.second << " ";
+        }
+
+        int e = k;
+        int s = 1;
+
+        while (e < arr.size()) {
+            cout << "going for index :" << e << endl;
+
+            // If max element goes out of window , pop it out
+            if (dq.front().first < s) {
+                dq.pop_front();
+            }
+
+            // Now adding the new number
+            while (!dq.empty() && dq.back().second < arr[e]) {
+                dq.pop_back();
+            }
+            //Add the element to queue
+            dq.emplace_back(e, arr[e]);
+            
+            //The bottom most(front) element of queue is the maximum element 
+            //so store in answer vecror/ArrayListr
+            if (!dq.empty())
+                ans.push_back(dq.front().second);
+            
+            //Shift Window
+            e++;
+            if(s<e)
+            s++;
+        }
+        return ans;
+    }
+};
