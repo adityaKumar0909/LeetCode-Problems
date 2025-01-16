03d99f31f06f06a6e93a5df4b6b672a0bc857382
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-16 21:31:45 +0530

    Time: 44 ms (28%), Space: 141.5 MB (27.92%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -7,38 +7,36 @@ public:
  
        
         deque<pair<int, int>> dq;
-        int index = 0;
-        int max = arr[0];
+        // int index = 0;
+        // int max = arr[0];
 
-        // Precomputing for first k window size
-        for (int i = 0; i < k; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                index = i;
-            }
-        }
+        // // Precomputing for first k window size
+        // for (int i = 0; i < k; i++) {
+        //     if (arr[i] > max) {
+        //         max = arr[i];
+        //         index = i;
+        //     }
+        // }
 
-        dq.emplace_back(index, arr[index]);
-        ans.push_back(arr[index]);
+        // dq.emplace_back(index, arr[index]);
+        // ans.push_back(arr[index]);
 
-        for (int i = index + 1; i < k; i++) {
-            while (!dq.empty() && dq.back().second < arr[i]) {
-                dq.pop_back();
-            }
+        // for (int i = index + 1; i < k; i++) {
+        //     while (!dq.empty() && dq.back().second < arr[i]) {
+        //         dq.pop_back();
+        //     }
 
-            dq.emplace_back(i, arr[i]);
-        }
+        //     dq.emplace_back(i, arr[i]);
+        // }
 
 
-        int e = k;
-        int s = 1;
+        int e = 0;
+        int s = 0;
 
-        while (e < arr.size()) {
+        while (e < arr.size())
+         {
 
-            // If max element goes out of window , pop it out
-            if (dq.front().first < s) {
-                dq.pop_front();
-            }
+            
 
             // Now adding the new number
             while (!dq.empty() && dq.back().second < arr[e]) {
@@ -46,16 +44,22 @@ public:
             }
             //Add the element to queue
             dq.emplace_back(e, arr[e]);
+
+            // If max element goes out of window , pop it out
+            if (dq.front().first < s ) {
+                dq.pop_front();
+            }
             
             //The bottom most(front) element of queue is the maximum element 
             //so store in answer vecror/ArrayListr
-            if (!dq.empty())
+            if (e-s+1==k){
                 ans.push_back(dq.front().second);
+                s++;
+            }
             
             //Shift Window
             e++;
-          
-            s++;
+
         }
         return ans;
     }
