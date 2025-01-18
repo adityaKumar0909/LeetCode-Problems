683ade3248f4edb9f6a7f2d7bd756e51d0d7a1ea
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-18 19:18:11 +0530

    Time: 0 ms (100%), Space: 35.7 MB (62.72%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -1,35 +1,46 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        
+        if(minutes==1 && customers.size()==1) return customers[0];
 
-        int Unsatisfied = 0;
-        for(int i = 0 ; i < minutes; i++){
-            Unsatisfied += customers[i]*grumpy[i];
+        // vector<int> ans;
+
+        int unsatisfiedCust = 0;
+        for(int i = 0 ; i < minutes ; i++){
+            unsatisfiedCust += customers[i]*grumpy[i];
         }
+        int maxUnsatisfied = unsatisfiedCust;
+        int index = 0;
 
-        int maxUnsatisfied = Unsatisfied;
-        int index=0;
-
-        for(int i = minutes ; i < customers.size() ; i++){
+        for(int i = minutes ; i< customers.size();i++){
             //Adding new unsatisfied customer
-            Unsatisfied += customers[i]*grumpy[i];
+            unsatisfiedCust += customers[i]*grumpy[i];
             //Removing old unsatisfied customer
-            Unsatisfied -= customers[i-minutes]*grumpy[i-minutes];
-
-            if(maxUnsatisfied < Unsatisfied){
-                maxUnsatisfied = Unsatisfied ;
+            unsatisfiedCust -= customers[i-minutes]*grumpy[i-minutes];
+            
+            if(maxUnsatisfied < unsatisfiedCust) {
+                maxUnsatisfied = unsatisfiedCust;
                 index = i;
             }
+
         }
 
-        int maxSatisfied = maxUnsatisfied;
-        int totalSatisfied = 0;
-        for(int i = 0 ; i < customers.size(); i++){
-            if(grumpy[i] == 0){
-                totalSatisfied += customers[i];
-            }
+        cout<<maxUnsatisfied<<endl;
+
+        int maxSatisfiedLater = maxUnsatisfied;
+        int satisfiedCust = 0;
+
+        for(int i=0;i<customers.size();i++){
+            if(grumpy[i]==0)
+            satisfiedCust+=customers[i];
         }
-        return maxSatisfied + totalSatisfied;
+        
+
+        cout<<satisfiedCust<<endl;
+
+        return satisfiedCust + maxSatisfiedLater;
+
 
     }
 };
