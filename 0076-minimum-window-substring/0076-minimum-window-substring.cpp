d7b8b98bdc4f0396cebf3ba5a1cd804e1b65a223
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-18 12:03:26 +0530

    Time: 20 ms (22.22%), Space: 11.5 MB (55.87%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -18,10 +18,9 @@ public:
         int start = 0;
         int end = 0;
         int k = 0; // This will be used to store minimum window size
-        int l = 0;
-
+  
         // Traverse the whole of S and find frequency of each letter in T
-        // for (int i = 0; i < s.length(); i++) 
+   
         while(end<s.length())
         {
 
@@ -56,52 +55,16 @@ public:
 
         }
 
-        // cout<<"Value of start : "<<start<<endl;
-        // cout<<"Value of k : "<<k<<endl;
-        // cout<<"Value of end : "<<end<<endl;
-
-
-
-
-        // cout<<minWindowSize<<endl;
-
-        // int start = 0;
-        // while (start < s.length() && (mpp1.find(s[start]) == mpp1.end() ||
-        //                               mpp2[s[start]] > mpp1[s[start]])) {
-        //     if (mpp1.find(s[start]) != mpp1.end()) {
-        //         mpp2[s[start]]--;
-        //     }
-        //     // if(start<s.length())
-        //     start++;
-        // }
-
-        // int end = s.length() - 1;
-        // while (end >= 0 && (mpp1.find(s[end]) == mpp1.end() || mpp2[s[end]] >
-        // mpp1[s[end]])) {
-        //     if (mpp1.find(s[end]) != mpp1.end()) {
-        //         mpp2[s[end]]--;
-        //     }
-        //     end--;
-        // }
-
+     
         // If start crosses end, no valid window exists
         if (start > end || minWindowSize ==INT_MAX) {
             return "";
         }
 
-        // string ans = "";
-        // for (int i = start; i <= k; i++) {
-        //     ans += s[i];
-        // }
+ 
 
         return s.substr(k,minWindowSize);
 
-        // for (auto x : mpp2) {
-        //     cout << x.first << " : " << x.second << endl;
-        // }
-
-        // return ans;
-
-        // cout << start << endl;
+       
     }
 };
