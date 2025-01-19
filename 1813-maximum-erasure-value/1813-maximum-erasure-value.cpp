6d33eecad0797e2bb85e2ef0ed55c3a41da1be4e
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-19 13:53:36 +0530

    Time: 575 ms (5.11%), Space: 96.1 MB (95.02%) - LeetHub

---
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+
+        int sum = 0;
+        int s = 0;
+        int e = 0;
+
+        vector<int> mpp(1e4+1,0);
+
+        int maxSum = INT_MIN;
+        while(e<nums.size()){
+            //If current number exists in window
+            if(mpp[nums[e]]>0){
+                maxSum = max(maxSum,sum);
+                cout<<maxSum<<endl;
+                // sum+=nums[e];
+            }
+            sum+=nums[e];
+            mpp[nums[e]]++;
+
+
+            while(mpp[nums[e]]>1){
+                sum-=nums[s];
+                mpp[nums[s]]--;
+                s++;
+            }
+
+
+        //    sum+=
+            e++;
+
+        }
+
+        // cout<<"sum:"<<sum<<endl;
+
+        
+
+        return maxSum>sum ? maxSum : sum;;
+
+
+        
+    }
+};
