317cfedd73f4e01f9ea801933cf7371a367527aa
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-21 19:31:14 +0530

    Time: 3 ms (66.71%), Space: 10.1 MB (42.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-
+        
+        //for storing frequency of each char in s1
         vector<int> freq(128, 0);
+        //for storing frequency of each char in s2
         vector<int> freq2(128, 0);
-
+        
+        //Find unique characters in s1
         int uniqueChar = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (freq[s1[i]] == 0)
@@ -22,27 +25,37 @@ public:
         while (end < s2.length()) {
 
             // Adding new char to window
+
+            //If char exist in s1 , increase it's freq in s2
             if (freq[s2[end]] > 0) {
 
                 freq2[s2[end]]++;
-
+                
+                //if freq of char in s1 and s2 matches increment count
                 if (freq2[s2[end]] == freq[s2[end]])
                     count++;
 
             }
 
             // Remove old elements
+            
+            //If window size greater s1 length
+
             if (end - start + 1 > s1.length()) {
-             
+                
+                //If char existed in s1 , decrease it's freq
                 if (freq[s2[start]] > 0) {
+                    //If freq of s1 and s2 are same 
                     if (freq[s2[start]] == freq2[s2[start]])
                         count--;
                 freq2[s2[start]]--;
 
                 }
+
                 start++;
             }
-
+            
+            //If we have reached our goal return true
             if (count == uniqueChar) {
                 return true;
             }
