46e19e893b93820ea205bf50d5a9461c749632a2
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-22 11:43:44 +0530

    Time: 5 ms (71.14%), Space: 11.6 MB (40.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+
         if (s.length() < t.length())
             return "";
 
@@ -18,24 +19,30 @@ public:
         int minStart = 0;
 
         while (end < s.length()) {
+
             char endChar = s[end];
-            if (mpp1[endChar] > 0) { 
-                if (mpp2[endChar] < mpp1[endChar]) {  
+
+            // If character is in T , increase it's freq in map2
+            if (mpp1[endChar] > 0) {
+                if (mpp2[endChar] < mpp1[endChar]) {
                     count++;
                 }
                 mpp2[endChar]++;
             }
 
+            // If count = required characters , count minWindow size
             while (count == required) {
+                // Find minimum window
                 if (end - start + 1 < minWindowSize) {
                     minWindowSize = end - start + 1;
                     minStart = start;
                 }
 
+                // If character exist in map1 reduce it's window
                 char startChar = s[start];
                 if (mpp1[startChar] > 0) {
                     mpp2[startChar]--;
-                    if (mpp2[startChar] < mpp1[startChar]) {  
+                    if (mpp2[startChar] < mpp1[startChar]) {
                         count--;
                     }
                 }
@@ -44,6 +51,6 @@ public:
             end++;
         }
 
-        return minWindowSize == INT_MAX ? "" : s.substr(minStart, minWindowSize);
+        return minWindowSize == INT_MAX ? "": s.substr(minStart, minWindowSize);
     }
 };
