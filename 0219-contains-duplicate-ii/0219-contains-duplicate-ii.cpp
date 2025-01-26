24479cfa401cc472416a6e5d4a7154c07de9032a
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-26 21:29:34 +0530

    Time: 51 ms (82.46%), Space: 81.3 MB (26.78%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,24 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> lastSeen; 
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (lastSeen.find(nums[i]) != lastSeen.end()) {
+                
+                if (i - lastSeen[nums[i]] <= k) {
+                    return true;
+                }
+            }
+            
+            lastSeen[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
