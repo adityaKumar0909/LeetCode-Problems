e874f081947afb75dfc2254f32c866ee2584d6c2
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-01-30 01:21:36 +0530

    Time: 7 ms (53.18%), Space: 11.6 MB (64.6%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -24,27 +24,35 @@ public:
 
         vector<int> ans;
 
-        while (end < s.length()) {
-
+        while (end < s.length()) { 
+            
+            //If char exist in mpp1
             if (mpp1[s[end]] >= 1) {
                 mpp2[s[end]]++;
 
+                //if freq in both maps same ( we have reached target for that specific alphabet)
                 if (mpp2[s[end]] == mpp1[s[end]]) {
                     count++;
                 }
             }
-
+            
+            //If window size greater than length of p
             while (end - start + 1 > p.length()) {
+
+                //If the character exist in mpp1
                 if (mpp1[s[start]] > 0) {
+                    
+                    //for case: cbca if you remove the first c , count wont be less 
                     if (mpp2[s[start]] == mpp1[s[start]])
                         count--;
-
+                    //decremenet freq of that character in mpp2
                     mpp2[s[start]]--;
                 }
 
                 start++;
             }
 
+            //If count in window reached totalcount of unique letters in p
             if (count == totalCount)
                 ans.push_back(start);
 
