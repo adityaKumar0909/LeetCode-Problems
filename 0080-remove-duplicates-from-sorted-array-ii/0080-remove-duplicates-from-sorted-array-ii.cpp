4fc7f1543dec2129f6c6c287af27011e6792e37e
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-02-12 22:06:56 +0530

    Time: 7 ms (30.98%), Space: 19.6 MB (10.88%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -2,44 +2,59 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        int size = nums.size();
+        //********This approach used O(N) space ****************
+
+        // int size = nums.size();
 
 
-        int p = 0;
-        int fast = 1;
-        int slow = size - 1;
+        // int p = 0;
+        // int fast = 1;
+        // int slow = size - 1;
 
-        vector<int> arr(size,0);
+        // vector<int> arr(size,0);
 
-        int counter = 1;
+        // int counter = 1;
 
-        arr[0] = nums[0];
+        // arr[0] = nums[0];
+
+        // for(int i=1;i<nums.size();i++){
+        //     //same character
+        //     if(nums[i]==nums[i-1]){
+        //         counter++;
+        //     }
+        //     else{
+        //         counter = 1;
+        //     }
+
+        //     if(counter<=2){
+        //         arr[fast] = nums[i];
+        //         fast++;
+        //     }
+        //     else if(counter>2){
+        //         arr[slow] = nums[i];
+        //         slow--;
+        //     }
+
+        // }
+
+        // nums = arr;
+
+
+
+        // return fast;
+
+        int pos = 1;
+        int count = 1;
+        int size =nums.size();
 
         for(int i=1;i<nums.size();i++){
-            //same character
-            if(nums[i]==nums[i-1]){
-                counter++;
-            }
-            else{
-                counter = 1;
-            }
+            if(nums[i-1] == nums[i]) count++;
+            else count = 1;
 
-            if(counter<=2){
-                arr[fast] = nums[i];
-                fast++;
-            }
-            else if(counter>2){
-                arr[slow] = nums[i];
-                slow--;
-            }
-
+            if(count<=2) nums[pos++] = nums[i];
         }
 
-        nums = arr;
-
-
-
-        return fast;
+        return pos;
         
     }
 };
