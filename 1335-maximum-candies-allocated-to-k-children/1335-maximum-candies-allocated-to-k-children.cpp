a506285473291f7c358d4d5f787602f2b36fb2ed
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-02-27 15:53:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    bool ifPossible(vector<int>candies,int num, int k){
+
+       int count = 0;
+       for(int i=candies.size()-1;i>=0;i--){
+        if(candies[i]>=num) count+=candies[i]/num;
+       }
+        cout<<count<<endl;
+        cout<<"for "<<num<<" "<<(count>=k)<<endl;
+        return count>=k;
+
+    }
+
+    int maximumCandies(vector<int>& candies, long long k) {
+
+        sort(candies.begin(),candies.end());
+        long long sum = 0;
+        for(auto x:candies)sum+=x;
+
+        int start = 1;
+        int end = sum/k;
+
+        cout<<"start:"<<start<<endl;
+        cout<<"end:"<<end<<endl;
+        cout<<endl;
+
+        if(end==0) return 0;
+        
+        while(start<=end){
+            int mid=start + (end-start)/2;
+
+            if(ifPossible(candies,mid,k)){
+                start=mid+1;
+
+            }
+            else{
+                end = mid-1;
+
+            }
+
+        }
+
+        cout<<"start:"<<start<<endl;
+        cout<<"end:"<<end<<endl;
+
+
+        return end;
+        
+    }
+};
