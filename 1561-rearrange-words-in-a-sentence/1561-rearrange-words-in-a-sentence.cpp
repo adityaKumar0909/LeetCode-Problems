1b7ca6f435a4d8f94a34ea0535bac178774a5f45
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-02 00:33:14 +0530

    Time: 14 ms (67.73%), Space: 22.6 MB (41.43%) - LeetHub

--- 1561-rearrange-words-in-a-sentence/1561-rearrange-words-in-a-sentence.cpp
+++ 1561-rearrange-words-in-a-sentence/1561-rearrange-words-in-a-sentence.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
-    static bool cmp(pair<string,int> &a,pair<string,int> &b){
-        int n = a.first.length();
-        int m = b.first.length();
-
-        if(n!=m) return n<m;
-        
-        return a.second<b.second;
-        
-
-
-    }
     string arrangeWords(string text) {
-        vector<pair<string , int>> pairs;
-        int n = text.length();
-        string word="";
-        int count=0;
-        text[0] =tolower(text[0]);
-        
-        for(int i=0;i<n;i++){
-            if(text[i]==' '){
-                i++;
-                pairs.push_back({word,count});
-                count++;
-                word="";
-            }
-            word+=text[i];
-        }
-        if(word!="")
-        pairs.push_back({word,count});
+        vector<string> words;
+        text[0] = tolower(text[0]); 
 
-        // for(auto x:pairs ){
-        //     cout<<x.first<<" "<<x.second<<endl;
-        // }
-        sort(pairs.begin(),pairs.end(),cmp);
-        string ans ="";
-        for(int i=0;i<pairs.size()-1;i++){
-            ans+=pairs[i].first+" ";
+        istringstream iss(text);
+        string word;
+        int count = 0;
+        
+        
+        while (iss >> word) {
+            words.push_back(word);
         }
-        ans+=pairs[pairs.size()-1].first;
-        ans[0] =toupper(ans[0]); 
+
+        
+        stable_sort(words.begin(), words.end(), [](const string &a, const string &b) {
+            return a.size() < b.size();
+        });
+
+
+        string ans = "";
+        for (string &w : words) {
+            ans += w + " ";
+        }
+
+        ans.pop_back();
+        ans[0] = toupper(ans[0]); 
 
         return ans;
-        
     }
 };
