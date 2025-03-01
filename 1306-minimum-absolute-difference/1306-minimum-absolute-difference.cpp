c6b418f80a352a834f62bc1544075983d72c523a
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-01 13:43:06 +0530

    Time: 15 ms (76.46%), Space: 36.8 MB (36.12%) - LeetHub

--- 1306-minimum-absolute-difference/1306-minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        // For storign answer
         vector<vector<int>> ans;
-
-        int minDiff = INT_MAX;
         int n = arr.size();
-        sort(arr.begin(), arr.end());
+        
+        sort(arr.begin(), arr.end());  
+        int minDiff = INT_MAX;
 
-        for (int i = 0; i < n - 1; i++) {
-            int diff = abs(arr[i + 1] - arr[i]);
-            minDiff = min(diff, minDiff);
-        }
-
-
-        for (int i = 0; i < n - 1; i++) {
-            int diff = abs(arr[i + 1] - arr[i]);
+        for (int i = 1; i < n; i++) {
+            int diff = arr[i] - arr[i - 1];  
+            
+            if (diff < minDiff) {
+                minDiff = diff;
+                ans.clear();  
+            }
             if (diff == minDiff) {
-                ans.push_back({arr[i],arr[i + 1]});
+                ans.push_back({arr[i - 1], arr[i]});
             }
         }
-
+        
         return ans;
     }
 };
