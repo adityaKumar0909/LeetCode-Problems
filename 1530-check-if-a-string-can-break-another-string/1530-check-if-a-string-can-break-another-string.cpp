fa2e76e48c504ea89c9acb8caaf69c5e6de5e3ae
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-01 18:21:35 +0530

    Time: 0 ms (100%), Space: 18.9 MB (7.05%) - LeetHub

--- 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.cpp
+++ 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
+    string countSort(string s) {
+        vector<int> freq(26, 0);
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        string sortedStr;
+        for (int i = 0; i < 26; i++) {
+            sortedStr.append(freq[i], 'a' + i);
+        }
+
+        return sortedStr;
+    }
+
     bool checkIfCanBreak(string s1, string s2) {
+        s1 = countSort(s1);
+        s2 = countSort(s2);
 
-        sort(s2.begin(),s2.end());
-        sort(s1.begin(),s1.end());
+        bool doesS2broke = true;
+        bool doesS1broke = true;
 
-
-
-        bool doesS2broke=true;
-        bool doesS1broke=true;
-
-
-        for(int i=0;i<s1.length();i++){
-            if(s1[i]>s2[i]) doesS2broke = false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1[i] > s2[i]) doesS2broke = false;
+            if (s1[i] < s2[i]) doesS1broke = false;
         }
 
-        for(int i=0;i<s1.length();i++){
-            if(s1[i]<s2[i]) doesS1broke = false;
-        }
-
-        if(doesS2broke || doesS1broke) return true;
-        else return false;
-
+        return doesS2broke || doesS1broke;
     }
 };
