b220c0778fb9940f2f7746106bf26f9a2925e94c
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-01 17:13:03 +0530

    Time: 11 ms (88.51%), Space: 103.6 MB (5.14%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
@@ -1,11 +1,32 @@
 class Solution {
 public:
+
+    vector<long> countSort(vector<int>&costs,int range){
+        vector<long> arr(range+1,0);
+        for(auto x:costs){
+            arr[x]++;
+        }
+
+        vector<long> ans;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>0){
+                for(int j=0;j<arr[i];j++){
+                    ans.push_back(i);
+                }
+            }
+        }
+
+        return ans;
+    }
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
+        int maximum=INT_MIN;
+        for(auto x:costs) maximum = max(maximum,x);
+
+        vector<long> newArr = countSort(costs,maximum);
         int count = 0;
-        int n=costs.size();
+        int n=newArr.size();
         for(int i=0;i<n;i++){
-            coins -= costs[i];
+            coins -= newArr[i];
             if(coins>=0) count++;
             if(coins<0) break;
         }
