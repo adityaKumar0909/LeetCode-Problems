5480134f97c4bac4c80e2920c37717e2b8c14378
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-01 14:49:00 +0530

    Time: 26 ms (70.32%), Space: 57.3 MB (28.21%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
@@ -10,7 +10,6 @@ public:
         int count=0;
 
         for(int i=n-2;i>=0;i-=2){
-            cout<<piles[i]<<endl;
             sum+=piles[i];
             count++;
             if(count==pile) break;
