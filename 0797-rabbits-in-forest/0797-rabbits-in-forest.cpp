48d683be162a2276ff6a87b2a493ed7622f5f60d
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-05 12:54:45 +0530

    Time: 0 ms (100%), Space: 12.2 MB (67.92%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -5,11 +5,11 @@ public:
         int count = 0;
         for(int i=0;i<answers.size();i++){
             if(freq[answers[i]] > 0){
-                cout<<"inside block 1"<<endl;
+                // cout<<"inside block 1"<<endl;
                 freq[answers[i]]--;
             }
             else{
-                cout<<"inside block 2"<<endl;
+                // cout<<"inside block 2"<<endl;
                 freq[answers[i]] = answers[i];
                 count += answers[i]+1;
             }
