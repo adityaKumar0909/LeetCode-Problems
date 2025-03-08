e90c16ca9dbcf2758a907674aea6f45a0e476dbb
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-08 16:16:10 +0530

    Time: 6 ms (1.96%), Space: 11 MB (83.05%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        // count frequencies of each
+        map<int, int> map;
+        for (auto x : arr1) {
+            map[x]++;
+        } 
+       
+        int n = arr1.size();
+        vector<int> ans;
+
+        for(auto &num:arr2){
+            while(map[num] > 0){
+            ans.push_back(num);
+            map[num]--;
+            }
+            map.erase(num);
+        }
+
+        for(auto &x:ans) cout<<x<<" ";
+
+        for(auto &x:map){
+            while(x.second-- > 0) {
+                ans.push_back(x.first);
+            }
+        }
+
+        return ans;
+
+    }
+};
