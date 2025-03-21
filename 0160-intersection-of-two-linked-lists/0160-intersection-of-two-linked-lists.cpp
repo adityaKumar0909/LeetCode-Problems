ca2c90a3ae59aebbb38d9342f49faa0da48278fe
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-21 23:13:21 +0530

    Time: 35 ms (70.71%), Space: 18.6 MB (53.96%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -10,22 +10,39 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 
-        unordered_set<ListNode*> list;
+        //connect tail of headA to head of LL2 and if they meet it's a common point;
+
         ListNode* temp = headA;
-        while(temp){
-            list.insert(temp);
+        while(temp->next){
             temp = temp->next;
         }
 
-        temp = headB;
-         while(temp){
-            if(list.find(temp)!=list.end())
-            return temp;
+        temp->next = headB;
 
-            temp = temp->next;
+        ListNode* slow = headA;
+        ListNode* fast = headA;
+
+        while(fast->next && fast->next->next){
+            
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                slow = headA;
+                while(slow!=fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                temp->next = NULL;
+                return slow;
+            }
         }
 
+
+        temp->next = NULL;
+        
         return NULL;
+
+
 
         
     }
