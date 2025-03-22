0fbad9be02b1b46eef59109622111e86f21fd80f
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-03-22 13:03:17 +0530

    Time: 23 ms (19.26%), Space: 42.4 MB (45.02%) - LeetHub

--- 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.cpp
+++ 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.cpp
@@ -17,61 +17,28 @@ public:
 
     long long minCost(vector<int>& nums, vector<int>& cost) {
 
-        // vector<int> prefix(nums.size(),0);
-        // prefix[0] = cost[0];
-        // int sum = 0;
-        // for(auto x:cost){
-        //     sum+=x;
-        // }
-
-        // int median = sum/2;
-
-        // int minEle = INT_MAX;
-        // int maxEle = INT_MIN;
-
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]<minEle) minEle = nums[i];
-        //     if(nums[i]>maxEle) maxEle = nums[i];
-        //     if(i!=0){
-        //         prefix[i] = prefix[i-1] + cost[i];
-        //     }
-
-        // }
-
         
+        int start = *min_element(nums.begin(), nums.end());
+        int end = *max_element(nums.begin(), nums.end());
 
-        // for(int i=0;i<prefix.size()-1;i++){
-        //     if(prefix[i] < median && median<prefix[i+1]) {
-        //         median = nums[i+1];
-        //         break;
-        //     }
-        // }
-
-
-        int start = 1;
-        int end = 1e6;
-
-        while(start<=end){
+        while(start<end){
 
             int mid = (start+end) / 2;
             long long sum1 = findCost(mid,nums,cost);
-            long long sum2 = findCost(mid-1,nums,cost);
-            long long sum3 = findCost(mid+1,nums,cost);
+            long long sum2 = findCost(mid+1,nums,cost);
 
-            if(sum2>= sum1 && sum1 <= sum3) return sum1;
 
             if(sum2<sum1){
-                end   = mid;
-                continue;
+                start   = mid+1;
+                
             }
 
-            if(sum3<sum1){
-                start = mid;
-                continue;
+            else{
+                end = mid;
             }
             
         }
-       return 0;
+       return findCost(start,nums,cost);
 
     }
 };
