46922fcc2016452f332fecaf52eaed3afef6bcb9
adityaKumar0909 <115136929+adityaKumar0909@users.noreply.github.com>
2025-04-11 22:22:24 +0530

    Time: 1 ms (23.03%), Space: 58.1 MB (24.34%) - LeetHub

---
+++ 2066-add-minimum-number-of-rungs/2066-add-minimum-number-of-rungs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int addRungs(vector<int>& rungs, int dist) {
+
+        int count = 0 ;
+        int size = rungs.size();
+         if(rungs[0] > dist)
+         count += (rungs[0] - 1 )/dist;
+
+         //cout<<count;
+
+        for(int i=1;i<size;i++){
+            if(rungs[i] -  rungs[i-1] > dist){
+                int val = (rungs[i] - rungs[i-1] -1 )/dist;
+                count+=val;
+            }
+        }
+
+        return count;
+        
+    }
+};
